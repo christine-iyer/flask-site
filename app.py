5f8710fb0d7a24c01fd5188dfd3fbf8a9fed8164
chrisIyer <hellohotbox420@gmail.com>
2025-07-07 19:12:20 -0400

    ok now we're good,

--- app.py
+++ app.py
@@ -4,12 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    image_url = url_for('static', filename='botanical-gin.png')
+    image_url0 = url_for('static', filename='botanical-gin.png')
+    image_url1 = url_for('static', filename='rose-gin.jpg')
+    image_url2 = url_for('static', filename='melon-gin.png')
     return f"""
     <html>
         <body>
-            <h4>Hello, Flask!</h4>
-            <img src="{image_url}" alt="Example Image" style="width: 200px; height: auto;">
+            <h2>Hello, Flask!</h2>
+            <h3>Here are some og out faves:</h3>
+            <p>We love these gins:</p>
+            <img src="{image_url0}" alt="Example Image" style="width: 200px; height: auto;">
+            <img src="{image_url1}" alt="Example Image" style="width: 200px; height: auto;">
+            <p>And my famous melon ball:</p>
+            <img src="{image_url2}" alt="Example Image" style="width: 200px; height: auto;">
         </body>
     </html>
     """
